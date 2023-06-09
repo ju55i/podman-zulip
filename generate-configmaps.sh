e73c85bf78a68362f5895f7a1e9d35fb882903bf
Jussi Talaskivi <jptalask@gmail.com>
2023-06-09 09:43:22 +0300

    Initial commit

---
+++ generate-configmaps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Export all variables in this shell
+set -a
+
+random_string() {
+    openssl rand -hex 16
+}
+
+POSTGRES_PASSWORD=$(random_string)
+MEMCACHED_PASSWORD=$(random_string)
+REDIS_PASSWORD=$(random_string)
+RABBITMQ_PASSWORD=$(random_string)
+ZULIP_SECRET_KEY=$(random_string)
+
+source zulip-vars.env
+
+envsubst < zulip-configmap-template.yaml > zulip-configmap.yaml
